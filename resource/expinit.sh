1086f11f91e31fdd4eb364142b8983e39c0c1a5e
sakuratimo <787886660@qq.com>
2020-04-14 19:14:53 +0800

    修改sleeptime为200

--- resource/expinit.sh
+++ resource/expinit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# cp /etc/apt/sources.list /etc/apt/sources.list.bak
-# cat > /etc/apt/sources.list <<EOF
-# # 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
-# deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
-# EOF
+cp /etc/apt/sources.list /etc/apt/sources.list.bak
+cat > /etc/apt/sources.list <<EOF
+# 默认注释了源码镜像以提高 apt update 速度，如有需要可自行取消注释
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic main restricted universe multiverse
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-updates main restricted universe multiverse
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-backports main restricted universe multiverse
+deb http://mirrors.tuna.tsinghua.edu.cn/ubuntu/ bionic-security main restricted universe multiverse
+EOF
 
 apt update
 apt install python3.6 -y

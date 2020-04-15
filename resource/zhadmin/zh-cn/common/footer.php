643fa16f3eec1ab16818db713ff6147812ef0c19
sakuratimo <787886660@qq.com>
2020-04-15 17:43:07 +0800

    回退版本

--- resource/zhadmin/zh-cn/common/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Text
-$_['text_footer']  = '技术支持 <a href="http://www.mycncart.com">MyCnCart</a>&nbsp;&nbsp;<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' 版权所有。';
-$_['text_version'] = '版本 %s';
--- resource/zhcus/zh-cn/extension/payment/alipay.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Text
-$_['text_title'] = '支付宝';
